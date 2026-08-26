75317c446ef018523119ffcf9ffb9e082dc4b46f
Charles Doan <charlestdoan2@gmail.com>
2026-08-25 23:15:42 -0500

    Added venice-specific request helper function, named functions accordingly, now planning to produce image edit requester function

--- server.py
+++ server.py
@@ -17,9 +17,7 @@ load_dotenv()
 api_key = os.getenv("VENICE_API_KEY")
 
 # helper 
-def get_image(prompt: str):
-    url = "https://api.venice.ai/api/v1/images/generations"
-
+def openai_image_request(url: str, prompt: str):
     payload = {
         "prompt": prompt,
         "background": "auto",
@@ -48,6 +46,62 @@ def get_image(prompt: str):
     with open("generated_image.png", "wb") as fh:
         fh.write(base64.b64decode(b64))
 
+def venice_image_request(url: str, prompt: str):
+    payload = {
+        "model": "seedream-v5-pro",
+        "prompt": prompt,
+        "cfg_scale": 7.5,
+        "embed_exif_metadata": False,
+        "format": "webp",
+        "height": 1024,
+        "hide_watermark": True,
+        "lora_strength": 50,
+        "negative_prompt": "",
+        "return_binary": False,
+        "variants": 3,
+        "safe_mode": False,
+        "seed": 0,
+        "steps": 8,
+        "style_preset": "3D Model",
+        "aspect_ratio": "1:1",
+        "resolution": "1K",
+        "quality": "high",
+        "enable_web_search": False,
+        "disable_prompt_optimization_thinking": False,
+        "enhance_prompt": False,
+        "width": 1024,
+    }
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    response.raise_for_status()
+
+    b64 = response.json()["images"][0]
+    with open("generated_image.png", "wb") as fh:
+        fh.write(base64.b64decode(b64))
 
 # runner 
-get_image("A beach with red roses")
+prompt = """
+<NEED NEW PROMPT>
+"""
+openAIUrl = f"{VENICE_BASE_URL}/images/generations"
+veniceUrl = f"{VENICE_BASE_URL}/image/generate"
+venice_image_request(veniceUrl, prompt)
+
+# @mcp.tool()
+# def get_image(prompt: str):
+#     """
+#     Based on the prompt, an image will be generated.
+
+#     1) HTTP request will be sent to the API.
+#     2) Response with base64 wrapped in a json will be received and saved as .png locally
+
+#     Args:
+#         prompt: description of the image to generate
+#     """
+#     url = f"{VENICE_BASE_URL}/images/generations"
+#     openai_image_request(url, prompt)
