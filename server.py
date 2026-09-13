8d2122066dd3ccd9c4e9407d468fff220cfe1c26
Charles Doan <charlestdoan2@gmail.com>
2026-09-13 16:28:57 -0500

    Added MCP tool-calling function

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def openai_image_request(url: str, prompt: str):
     payload = {
         "prompt": prompt,
         "background": "auto",
-        "model": "venice-sd35",
+        "model": "TODO",
         "moderation": "low",
         "n": 1,
         "output_compression": 100,
@@ -84,24 +84,27 @@ def venice_image_request(url: str, prompt: str):
     with open("generated_image.png", "wb") as fh:
         fh.write(base64.b64decode(b64))
 
+
+@mcp.tool()
+def get_venice_image(prompt: str):
+    """
+    Based on the prompt, an image will be generated.
+
+    1) HTTP request will be sent to the API.
+    2) Response with base64 wrapped in a json will be received and saved as .png locally
+
+    Args:
+        prompt: description of the image to generate
+    """
+    veniceUrl = f"{VENICE_BASE_URL}/image/generate"
+    venice_image_request(veniceUrl, prompt)
+
 # runner 
 prompt = """
 <NEED NEW PROMPT>
 """
 openAIUrl = f"{VENICE_BASE_URL}/images/generations"
 veniceUrl = f"{VENICE_BASE_URL}/image/generate"
-venice_image_request(veniceUrl, prompt)
 
-# @mcp.tool()
-# def get_image(prompt: str):
-#     """
-#     Based on the prompt, an image will be generated.
-
-#     1) HTTP request will be sent to the API.
-#     2) Response with base64 wrapped in a json will be received and saved as .png locally
-
-#     Args:
-#         prompt: description of the image to generate
-#     """
-#     url = f"{VENICE_BASE_URL}/images/generations"
-#     openai_image_request(url, prompt)
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
